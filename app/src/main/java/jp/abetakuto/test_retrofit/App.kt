dbe7da87eecf16a98eb4805ee420098e17558877
kmachida12345 <kmachida12345@gmail.com>
2021-03-13 21:09:51 +0900

    Fix: fix initialization Timber

--- app/src/main/java/jp/abetakuto/test_retrofit/App.kt
+++ app/src/main/java/jp/abetakuto/test_retrofit/App.kt
@@ -8,6 +8,6 @@ import timber.log.Timber
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) Timber.forest()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
